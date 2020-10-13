00e1370dc391d401756758213aa8b344ac40fe0c
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2020-10-13 11:07:53 +0200

    add exercise

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
 'use strict'
+// variables declaradas
+const text = document.querySelector(".text");
+const button = document.querySelector(".button");
+const paragraph1 = document.querySelector(".paragraph1");
+const paragraph2 = document.querySelector(".paragraph2");
 
-
-
-
-
-
-
-
+// 1. Generar numero ramdom
 function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
+return Math.ceil(Math.random() * max);
+ }
+ const ramdon = getRandomNumber(100);
+console.log (`mi número aleatorio es ${ramdom})`;
+
+// 2. Evento botón - número
+function updateClick() {
+const textValue
 }
+btn.addEventListener("click", updateclick);
 
 
 
+//  3. Cambiar mensaje paragraph1
+ const showMessage = function () {
+  const textValue = parseInt(text.value);
+  if () {
+
+  } else if {
+
+  }else if {
+
+  }else if{
+
+  }else{
+
+  }
+paragraph1.addEventListener("change", showMessage);
+
+
+
+
+
+
+// 1.Necesito numero ramdon 
+// 2.if/else con value para los mensajes que tienen que aparecer en los paragraph con el ${ramdom}
+// 3.Eventos en todo lo que interactua en el ejercicio/constantes.
